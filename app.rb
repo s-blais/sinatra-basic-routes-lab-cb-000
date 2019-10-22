5bfd5f58da8abcbdf5b16b731e973445acd3fb51
Stephen Blais <stephen@s-blais.com>
2019-10-22 19:10:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Stephen"
+  end
+
+  get '/hometown' do
+    "My hometown is Slatersville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is All the Same"
+  end
+
 end
