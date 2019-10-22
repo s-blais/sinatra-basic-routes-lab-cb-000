b35bdf415d6567f54ca5a61baea8b4b93d2316e6
Stephen Blais <stephen@s-blais.com>
2019-10-22 19:06:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is "
+    "My name is Stephen"
   end
 
   get '/hometown' do
-    "My hometown is "
+    "My hometown is Slatersville"
   end
 
   get '/favorite-song' do
-    "My favorite song is "
+    "My favorite song is All the Same"
   end
 
 end
